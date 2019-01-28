a5445a8542d2140e4d6726d046d6053f52361836
Travis Hoki <thoki@overstock.com>
2019-01-27 20:19:18 -0700

    set initial exercise

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// import App from './Step1/Exercise/App';
+import App from './Step1/Exercise/App';
 // import App from './Step1/Solution/App';
 
-import App from './Step2/Exercise/App';
+// import App from './Step2/Exercise/App';
 // import App from './Step2/Solution/App';
 
 // import App from './Step3/Exercise/App';
